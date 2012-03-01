259359a33c09dc7f75dfafb0e6c3b3787dbe03e1
Stephan Wahlbrink <sw@wahlbrink.eu>
2012-03-01 20:06:40 +0100

    [*] Adapted to support workbench theme
    Signed-off-by: Stephan Wahlbrink <sw@wahlbrink.eu>

--- de.walware.docmlet.tex.ui/src/de/walware/docmlet/tex/internal/ui/TexUIPreferenceInitializer.java
+++ de.walware.docmlet.tex.ui/src/de/walware/docmlet/tex/internal/ui/TexUIPreferenceInitializer.java
@@ -20,8 +20,8 @@ import static de.walware.ecommons.text.ui.presentation.ITextPresentationConstant
 
 import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
 import org.eclipse.core.runtime.preferences.DefaultScope;
+import org.eclipse.core.runtime.preferences.IEclipsePreferences;
 import org.eclipse.jface.preference.IPreferenceStore;
-import org.eclipse.swt.graphics.RGB;
 import org.eclipse.ui.editors.text.EditorsUI;
 
 import de.walware.ecommons.ltk.ui.sourceediting.ISmartInsertSettings.TabAction;
@@ -29,6 +29,9 @@ import de.walware.ecommons.ltk.ui.sourceediting.assist.ContentAssistComputerRegi
 import de.walware.ecommons.preferences.PreferencesUtil;
 import de.walware.ecommons.text.ui.settings.AssistPreferences;
 import de.walware.ecommons.text.ui.settings.DecorationPreferences;
+
+import de.walware.workbench.ui.IWaThemeConstants;
+import de.walware.workbench.ui.util.ThemeUtil;
 
 import de.walware.docmlet.tex.ui.TexUIPreferences;
 import de.walware.docmlet.tex.ui.editors.LtxEditorBuild;
@@ -47,102 +50,102 @@ public class TexUIPreferenceInitializer extends AbstractPreferenceInitializer {
 	public void initializeDefaultPreferences() {
 		final IPreferenceStore store = TexUIPlugin.getDefault().getPreferenceStore();
 		final DefaultScope defaultScope = new DefaultScope();
+		final IEclipsePreferences pref = defaultScope.getNode(TexUIPlugin.PLUGIN_ID);
+		final ThemeUtil theme = new ThemeUtil();
 		
 		EditorsUI.useAnnotationsPreferencePage(store);
 		EditorsUI.useQuickDiffPreferencePage(store);
 		
-		store.setDefault(ITexTextStyles.TS_DEFAULT + TEXTSTYLE_COLOR_SUFFIX, "0,0,0"); //$NON-NLS-1$
-		store.setDefault(ITexTextStyles.TS_DEFAULT + TEXTSTYLE_BOLD_SUFFIX, false);
-		store.setDefault(ITexTextStyles.TS_DEFAULT + TEXTSTYLE_ITALIC_SUFFIX, false);
-		store.setDefault(ITexTextStyles.TS_DEFAULT + TEXTSTYLE_UNDERLINE_SUFFIX, false);
-		store.setDefault(ITexTextStyles.TS_DEFAULT + TEXTSTYLE_STRIKETHROUGH_SUFFIX, false);
+		String color;
+		pref.put(ITexTextStyles.TS_DEFAULT + TEXTSTYLE_COLOR_SUFFIX, theme.getColorPrefValue(IWaThemeConstants.CODE_DEFAULT_COLOR));
+		pref.putBoolean(ITexTextStyles.TS_DEFAULT + TEXTSTYLE_BOLD_SUFFIX, false);
+		pref.putBoolean(ITexTextStyles.TS_DEFAULT + TEXTSTYLE_ITALIC_SUFFIX, false);
+		pref.putBoolean(ITexTextStyles.TS_DEFAULT + TEXTSTYLE_UNDERLINE_SUFFIX, false);
+		pref.putBoolean(ITexTextStyles.TS_DEFAULT + TEXTSTYLE_STRIKETHROUGH_SUFFIX, false);
 		
-		store.setDefault(ITexTextStyles.TS_CONTROL_WORD + TEXTSTYLE_COLOR_SUFFIX, "127,0,255"); //$NON-NLS-1$
-		store.setDefault(ITexTextStyles.TS_CONTROL_WORD + TEXTSTYLE_BOLD_SUFFIX, false);
-		store.setDefault(ITexTextStyles.TS_CONTROL_WORD + TEXTSTYLE_ITALIC_SUFFIX, false);
-		store.setDefault(ITexTextStyles.TS_CONTROL_WORD + TEXTSTYLE_UNDERLINE_SUFFIX, false);
-		store.setDefault(ITexTextStyles.TS_CONTROL_WORD + TEXTSTYLE_STRIKETHROUGH_SUFFIX, false);
+		pref.put(ITexTextStyles.TS_CONTROL_WORD + TEXTSTYLE_COLOR_SUFFIX, theme.getColorPrefValue(IWaThemeConstants.CODE_DOC_COMMAND_COLOR));
+		pref.putBoolean(ITexTextStyles.TS_CONTROL_WORD + TEXTSTYLE_BOLD_SUFFIX, false);
+		pref.putBoolean(ITexTextStyles.TS_CONTROL_WORD + TEXTSTYLE_ITALIC_SUFFIX, false);
+		pref.putBoolean(ITexTextStyles.TS_CONTROL_WORD + TEXTSTYLE_UNDERLINE_SUFFIX, false);
+		pref.putBoolean(ITexTextStyles.TS_CONTROL_WORD + TEXTSTYLE_STRIKETHROUGH_SUFFIX, false);
 		
-		store.setDefault(ITexTextStyles.TS_CONTROL_WORD_SUB_SECTIONING + TEXTSTYLE_COLOR_SUFFIX, "127,0,255"); //$NON-NLS-1$
-		store.setDefault(ITexTextStyles.TS_CONTROL_WORD_SUB_SECTIONING + TEXTSTYLE_BOLD_SUFFIX, true);
-		store.setDefault(ITexTextStyles.TS_CONTROL_WORD_SUB_SECTIONING + TEXTSTYLE_ITALIC_SUFFIX, false);
-		store.setDefault(ITexTextStyles.TS_CONTROL_WORD_SUB_SECTIONING + TEXTSTYLE_UNDERLINE_SUFFIX, false);
-		store.setDefault(ITexTextStyles.TS_CONTROL_WORD_SUB_SECTIONING + TEXTSTYLE_STRIKETHROUGH_SUFFIX, false);
+		pref.put(ITexTextStyles.TS_CONTROL_WORD_SUB_SECTIONING + TEXTSTYLE_COLOR_SUFFIX, theme.getColorPrefValue("de.walware.docmlet.themes.SectionColor")); //$NON-NLS-1$
+		pref.putBoolean(ITexTextStyles.TS_CONTROL_WORD_SUB_SECTIONING + TEXTSTYLE_BOLD_SUFFIX, true);
+		pref.putBoolean(ITexTextStyles.TS_CONTROL_WORD_SUB_SECTIONING + TEXTSTYLE_ITALIC_SUFFIX, false);
+		pref.putBoolean(ITexTextStyles.TS_CONTROL_WORD_SUB_SECTIONING + TEXTSTYLE_UNDERLINE_SUFFIX, false);
+		pref.putBoolean(ITexTextStyles.TS_CONTROL_WORD_SUB_SECTIONING + TEXTSTYLE_STRIKETHROUGH_SUFFIX, false);
 		
-		store.setDefault(ITexTextStyles.TS_CONTROL_CHAR + TEXTSTYLE_COLOR_SUFFIX, "63,0,127"); //$NON-NLS-1$
-		store.setDefault(ITexTextStyles.TS_CONTROL_CHAR + TEXTSTYLE_BOLD_SUFFIX, false);
-		store.setDefault(ITexTextStyles.TS_CONTROL_CHAR + TEXTSTYLE_ITALIC_SUFFIX, false);
-		store.setDefault(ITexTextStyles.TS_CONTROL_CHAR + TEXTSTYLE_UNDERLINE_SUFFIX, false);
-		store.setDefault(ITexTextStyles.TS_CONTROL_CHAR + TEXTSTYLE_STRIKETHROUGH_SUFFIX, false);
+		pref.put(ITexTextStyles.TS_CONTROL_CHAR + TEXTSTYLE_COLOR_SUFFIX, theme.getColorPrefValue(IWaThemeConstants.CODE_DOC_COMMAND_SPECIAL_COLOR));
+		pref.putBoolean(ITexTextStyles.TS_CONTROL_CHAR + TEXTSTYLE_BOLD_SUFFIX, false);
+		pref.putBoolean(ITexTextStyles.TS_CONTROL_CHAR + TEXTSTYLE_ITALIC_SUFFIX, false);
+		pref.putBoolean(ITexTextStyles.TS_CONTROL_CHAR + TEXTSTYLE_UNDERLINE_SUFFIX, false);
+		pref.putBoolean(ITexTextStyles.TS_CONTROL_CHAR + TEXTSTYLE_STRIKETHROUGH_SUFFIX, false);
 		
-		store.setDefault(ITexTextStyles.TS_CURLY_BRACKETS + TEXTSTYLE_COLOR_SUFFIX, "0,0,127"); //$NON-NLS-1$
-		store.setDefault(ITexTextStyles.TS_CURLY_BRACKETS + TEXTSTYLE_BOLD_SUFFIX, true);
-		store.setDefault(ITexTextStyles.TS_CURLY_BRACKETS + TEXTSTYLE_ITALIC_SUFFIX, false);
-		store.setDefault(ITexTextStyles.TS_CURLY_BRACKETS + TEXTSTYLE_UNDERLINE_SUFFIX, false);
-		store.setDefault(ITexTextStyles.TS_CURLY_BRACKETS + TEXTSTYLE_STRIKETHROUGH_SUFFIX, false);
+		color = theme.getColorPrefValue(IWaThemeConstants.CODE_SUB_COLOR);
+		pref.put(ITexTextStyles.TS_CURLY_BRACKETS + TEXTSTYLE_COLOR_SUFFIX, color);
+		pref.putBoolean(ITexTextStyles.TS_CURLY_BRACKETS + TEXTSTYLE_BOLD_SUFFIX, true);
+		pref.putBoolean(ITexTextStyles.TS_CURLY_BRACKETS + TEXTSTYLE_ITALIC_SUFFIX, false);
+		pref.putBoolean(ITexTextStyles.TS_CURLY_BRACKETS + TEXTSTYLE_UNDERLINE_SUFFIX, false);
+		pref.putBoolean(ITexTextStyles.TS_CURLY_BRACKETS + TEXTSTYLE_STRIKETHROUGH_SUFFIX, false);
 		
-//		store.setDefault(ITexTextStyles.TS_SQUARED_BRACKETS + TEXTSTYLE_COLOR_SUFFIX, "0,0,127"); //$NON-NLS-1$
-//		store.setDefault(ITexTextStyles.TS_SQUARED_BRACKETS + TEXTSTYLE_BOLD_SUFFIX, false);
-//		store.setDefault(ITexTextStyles.TS_SQUARED_BRACKETS + TEXTSTYLE_ITALIC_SUFFIX, false);
-//		store.setDefault(ITexTextStyles.TS_SQUARED_BRACKETS + TEXTSTYLE_UNDERLINE_SUFFIX, false);
-//		store.setDefault(ITexTextStyles.TS_SQUARED_BRACKETS + TEXTSTYLE_STRIKETHROUGH_SUFFIX, false);
+//		pref.put(ITexTextStyles.TS_SQUARED_BRACKETS + TEXTSTYLE_COLOR_SUFFIX, color);
+//		pref.putBoolean(ITexTextStyles.TS_SQUARED_BRACKETS + TEXTSTYLE_BOLD_SUFFIX, false);
+//		pref.putBoolean(ITexTextStyles.TS_SQUARED_BRACKETS + TEXTSTYLE_ITALIC_SUFFIX, false);
+//		pref.putBoolean(ITexTextStyles.TS_SQUARED_BRACKETS + TEXTSTYLE_UNDERLINE_SUFFIX, false);
+//		pref.putBoolean(ITexTextStyles.TS_SQUARED_BRACKETS + TEXTSTYLE_STRIKETHROUGH_SUFFIX, false);
 //		
-		store.setDefault(ITexTextStyles.TS_MATH + TEXTSTYLE_COLOR_SUFFIX, "175,95,95"); //$NON-NLS-1$
-		store.setDefault(ITexTextStyles.TS_MATH + TEXTSTYLE_BOLD_SUFFIX, false);
-		store.setDefault(ITexTextStyles.TS_MATH + TEXTSTYLE_ITALIC_SUFFIX, false);
-		store.setDefault(ITexTextStyles.TS_MATH + TEXTSTYLE_UNDERLINE_SUFFIX, false);
-		store.setDefault(ITexTextStyles.TS_MATH + TEXTSTYLE_STRIKETHROUGH_SUFFIX, false);
+		pref.put(ITexTextStyles.TS_MATH + TEXTSTYLE_COLOR_SUFFIX, theme.getColorPrefValue(IWaThemeConstants.CODE_DOC_2ND_COLOR));
+		pref.putBoolean(ITexTextStyles.TS_MATH + TEXTSTYLE_BOLD_SUFFIX, false);
+		pref.putBoolean(ITexTextStyles.TS_MATH + TEXTSTYLE_ITALIC_SUFFIX, false);
+		pref.putBoolean(ITexTextStyles.TS_MATH + TEXTSTYLE_UNDERLINE_SUFFIX, false);
+		pref.putBoolean(ITexTextStyles.TS_MATH + TEXTSTYLE_STRIKETHROUGH_SUFFIX, false);
 		
-		store.setDefault(ITexTextStyles.TS_MATH_CONTROL_WORD + TEXTSTYLE_COLOR_SUFFIX, "159,63,127"); //$NON-NLS-1$
-		store.setDefault(ITexTextStyles.TS_MATH_CONTROL_WORD + TEXTSTYLE_BOLD_SUFFIX, false);
-		store.setDefault(ITexTextStyles.TS_MATH_CONTROL_WORD + TEXTSTYLE_ITALIC_SUFFIX, false);
-		store.setDefault(ITexTextStyles.TS_MATH_CONTROL_WORD + TEXTSTYLE_UNDERLINE_SUFFIX, false);
-		store.setDefault(ITexTextStyles.TS_MATH_CONTROL_WORD + TEXTSTYLE_STRIKETHROUGH_SUFFIX, false);
+		pref.put(ITexTextStyles.TS_MATH_CONTROL_WORD + TEXTSTYLE_COLOR_SUFFIX, theme.getColorPrefValue(IWaThemeConstants.CODE_DOC_2ND_COMMAND_COLOR));
+		pref.putBoolean(ITexTextStyles.TS_MATH_CONTROL_WORD + TEXTSTYLE_BOLD_SUFFIX, false);
+		pref.putBoolean(ITexTextStyles.TS_MATH_CONTROL_WORD + TEXTSTYLE_ITALIC_SUFFIX, false);
+		pref.putBoolean(ITexTextStyles.TS_MATH_CONTROL_WORD + TEXTSTYLE_UNDERLINE_SUFFIX, false);
+		pref.putBoolean(ITexTextStyles.TS_MATH_CONTROL_WORD + TEXTSTYLE_STRIKETHROUGH_SUFFIX, false);
 		
-		store.setDefault(ITexTextStyles.TS_MATH_CONTROL_CHAR + TEXTSTYLE_USE_SUFFIX, ITexTextStyles.TS_MATH_CONTROL_WORD);
+		pref.put(ITexTextStyles.TS_MATH_CONTROL_CHAR + TEXTSTYLE_USE_SUFFIX, ITexTextStyles.TS_MATH_CONTROL_WORD);
 		
-		store.setDefault(ITexTextStyles.TS_MATH_CURLY_BRACKETS + TEXTSTYLE_COLOR_SUFFIX, "159,95,127"); //$NON-NLS-1$
-		store.setDefault(ITexTextStyles.TS_MATH_CURLY_BRACKETS + TEXTSTYLE_BOLD_SUFFIX, true);
-		store.setDefault(ITexTextStyles.TS_MATH_CURLY_BRACKETS + TEXTSTYLE_ITALIC_SUFFIX, false);
-		store.setDefault(ITexTextStyles.TS_MATH_CURLY_BRACKETS + TEXTSTYLE_UNDERLINE_SUFFIX, false);
-		store.setDefault(ITexTextStyles.TS_MATH_CURLY_BRACKETS + TEXTSTYLE_STRIKETHROUGH_SUFFIX, false);
+		pref.put(ITexTextStyles.TS_MATH_CURLY_BRACKETS + TEXTSTYLE_COLOR_SUFFIX, theme.getColorPrefValue(IWaThemeConstants.CODE_DOC_2ND_SUB_COLOR));
+		pref.putBoolean(ITexTextStyles.TS_MATH_CURLY_BRACKETS + TEXTSTYLE_BOLD_SUFFIX, true);
+		pref.putBoolean(ITexTextStyles.TS_MATH_CURLY_BRACKETS + TEXTSTYLE_ITALIC_SUFFIX, false);
+		pref.putBoolean(ITexTextStyles.TS_MATH_CURLY_BRACKETS + TEXTSTYLE_UNDERLINE_SUFFIX, false);
+		pref.putBoolean(ITexTextStyles.TS_MATH_CURLY_BRACKETS + TEXTSTYLE_STRIKETHROUGH_SUFFIX, false);
 		
-		store.setDefault(ITexTextStyles.TS_COMMENT + TEXTSTYLE_COLOR_SUFFIX, "63,127,79"); //$NON-NLS-1$
-		store.setDefault(ITexTextStyles.TS_COMMENT + TEXTSTYLE_BOLD_SUFFIX, false);
-		store.setDefault(ITexTextStyles.TS_COMMENT + TEXTSTYLE_ITALIC_SUFFIX, false);
-		store.setDefault(ITexTextStyles.TS_COMMENT + TEXTSTYLE_UNDERLINE_SUFFIX, false);
-		store.setDefault(ITexTextStyles.TS_COMMENT + TEXTSTYLE_STRIKETHROUGH_SUFFIX, false);
+		pref.put(ITexTextStyles.TS_VERBATIM + TEXTSTYLE_COLOR_SUFFIX, theme.getColorPrefValue(IWaThemeConstants.CODE_VERBATIM_COLOR));
+		pref.putBoolean(ITexTextStyles.TS_VERBATIM + TEXTSTYLE_BOLD_SUFFIX, false);
+		pref.putBoolean(ITexTextStyles.TS_VERBATIM + TEXTSTYLE_ITALIC_SUFFIX, false);
+		pref.putBoolean(ITexTextStyles.TS_VERBATIM + TEXTSTYLE_UNDERLINE_SUFFIX, false);
+		pref.putBoolean(ITexTextStyles.TS_VERBATIM + TEXTSTYLE_STRIKETHROUGH_SUFFIX, false);
 		
-		store.setDefault(ITexTextStyles.TS_TASK_TAG + TEXTSTYLE_COLOR_SUFFIX, "63,127,95"); //$NON-NLS-1$
-		store.setDefault(ITexTextStyles.TS_TASK_TAG + TEXTSTYLE_BOLD_SUFFIX, true);
-		store.setDefault(ITexTextStyles.TS_TASK_TAG + TEXTSTYLE_ITALIC_SUFFIX, false);
-		store.setDefault(ITexTextStyles.TS_TASK_TAG + TEXTSTYLE_UNDERLINE_SUFFIX, false);
-		store.setDefault(ITexTextStyles.TS_TASK_TAG + TEXTSTYLE_STRIKETHROUGH_SUFFIX, false);
+		pref.put(ITexTextStyles.TS_COMMENT + TEXTSTYLE_COLOR_SUFFIX, theme.getColorPrefValue(IWaThemeConstants.CODE_COMMENT_COLOR));
+		pref.putBoolean(ITexTextStyles.TS_COMMENT + TEXTSTYLE_BOLD_SUFFIX, false);
+		pref.putBoolean(ITexTextStyles.TS_COMMENT + TEXTSTYLE_ITALIC_SUFFIX, false);
+		pref.putBoolean(ITexTextStyles.TS_COMMENT + TEXTSTYLE_UNDERLINE_SUFFIX, false);
+		pref.putBoolean(ITexTextStyles.TS_COMMENT + TEXTSTYLE_STRIKETHROUGH_SUFFIX, false);
 		
-		store.setDefault(ITexTextStyles.TS_VERBATIM + TEXTSTYLE_COLOR_SUFFIX, "79,79,79"); //$NON-NLS-1$
-		store.setDefault(ITexTextStyles.TS_VERBATIM + TEXTSTYLE_BOLD_SUFFIX, false);
-		store.setDefault(ITexTextStyles.TS_VERBATIM + TEXTSTYLE_ITALIC_SUFFIX, true);
-		store.setDefault(ITexTextStyles.TS_VERBATIM + TEXTSTYLE_UNDERLINE_SUFFIX, false);
-		store.setDefault(ITexTextStyles.TS_VERBATIM + TEXTSTYLE_STRIKETHROUGH_SUFFIX, false);
+		pref.put(ITexTextStyles.TS_TASK_TAG + TEXTSTYLE_COLOR_SUFFIX, theme.getColorPrefValue(IWaThemeConstants.CODE_COMMENT_TASKTAG_COLOR));
+		pref.putBoolean(ITexTextStyles.TS_TASK_TAG + TEXTSTYLE_BOLD_SUFFIX, true);
+		pref.putBoolean(ITexTextStyles.TS_TASK_TAG + TEXTSTYLE_ITALIC_SUFFIX, false);
+		pref.putBoolean(ITexTextStyles.TS_TASK_TAG + TEXTSTYLE_UNDERLINE_SUFFIX, false);
+		pref.putBoolean(ITexTextStyles.TS_TASK_TAG + TEXTSTYLE_STRIKETHROUGH_SUFFIX, false);
 		
-		defaultScope.getNode(TexUIPlugin.TEX_EDITOR_QUALIFIER).put(ContentAssistComputerRegistry.CIRCLING_ORDERED, "tex-elements:false,templates:true,paths:true"); //$NON-NLS-1$
-		defaultScope.getNode(TexUIPlugin.TEX_EDITOR_QUALIFIER).put(ContentAssistComputerRegistry.DEFAULT_DISABLED, ""); //$NON-NLS-1$
+		final IEclipsePreferences editorNode = defaultScope.getNode(TexUIPlugin.TEX_EDITOR_QUALIFIER);
+		editorNode.put(ContentAssistComputerRegistry.CIRCLING_ORDERED, "tex-elements:false,templates:true,paths:true"); //$NON-NLS-1$
+		editorNode.put(ContentAssistComputerRegistry.DEFAULT_DISABLED, ""); //$NON-NLS-1$
 		
 		// EditorPreferences
-		final DecorationPreferences decoPrefs = TexUIPreferences.EDITING_DECO_PREFERENCES;
-		PreferencesUtil.setPrefValue(defaultScope, decoPrefs.getMatchingBracketsEnabled(), Boolean.TRUE);
-		PreferencesUtil.setPrefValue(defaultScope, decoPrefs.getMatchingBracketsColor(), new RGB(192, 192, 192));
+		pref.putBoolean(DecorationPreferences.MATCHING_BRACKET_ENABLED_KEY, true);
+		pref.put(DecorationPreferences.MATCHING_BRACKET_COLOR_KEY, theme.getColorPrefValue(IWaThemeConstants.MATCHING_BRACKET_COLOR));
 		
-		final AssistPreferences assistPrefs = TexUIPreferences.EDITING_ASSIST_PREFERENCES;
-		PreferencesUtil.setPrefValue(defaultScope, assistPrefs.getAutoActivationEnabledPref(), Boolean.TRUE);
-		PreferencesUtil.setPrefValue(defaultScope, assistPrefs.getAutoActivationDelayPref(), 200);
-		PreferencesUtil.setPrefValue(defaultScope, assistPrefs.getAutoInsertSinglePref(), Boolean.FALSE);
-		PreferencesUtil.setPrefValue(defaultScope, assistPrefs.getAutoInsertPrefixPref(), Boolean.FALSE);
-		PreferencesUtil.setPrefValue(defaultScope, assistPrefs.getProposalsBackgroundPref(), new RGB(243, 247, 255));
-		PreferencesUtil.setPrefValue(defaultScope, assistPrefs.getProposalsForegroundPref(), new RGB(0, 0, 0));
-		PreferencesUtil.setPrefValue(defaultScope, assistPrefs.getInformationBackgroundPref(), new RGB(255, 255, 255));
-		PreferencesUtil.setPrefValue(defaultScope, assistPrefs.getInformationForegroundPref(), new RGB(0, 0, 0));
-		
+		{	final AssistPreferences assistPrefs = TexUIPreferences.EDITING_ASSIST_PREFERENCES;
+			PreferencesUtil.setPrefValue(defaultScope, assistPrefs.getAutoActivationEnabledPref(), Boolean.TRUE);
+			PreferencesUtil.setPrefValue(defaultScope, assistPrefs.getAutoActivationDelayPref(), 200);
+			PreferencesUtil.setPrefValue(defaultScope, assistPrefs.getAutoInsertSinglePref(), Boolean.FALSE);
+			PreferencesUtil.setPrefValue(defaultScope, assistPrefs.getAutoInsertPrefixPref(), Boolean.FALSE);
+		}
 		PreferencesUtil.setPrefValue(defaultScope, TexEditorOptions.FOLDING_ENABLED_PREF, Boolean.TRUE); 
 		PreferencesUtil.setPrefValue(defaultScope, TexEditorOptions.MARKOCCURRENCES_ENABLED_PREF, Boolean.TRUE);
 		
