ca748a970422e6bf0c6dd9040f02d2c7a3dcc89a
Stephan Wahlbrink <sw@wahlbrink.eu>
2016-02-19 10:00:50 +0100

    Updated copyright for 2016
    Signed-off-by: Stephan Wahlbrink <sw@wahlbrink.eu>

--- de.walware.docmlet.tex.ui/src/de/walware/docmlet/tex/internal/ui/editors/HardLineWrap.java
+++ de.walware.docmlet.tex.ui/src/de/walware/docmlet/tex/internal/ui/editors/HardLineWrap.java
@@ -1,5 +1,5 @@
 /*=============================================================================#
- # Copyright (c) 2004-2015 TeXlipse-Project (texlipse.sf.net) and others.
+ # Copyright (c) 2004-2016 TeXlipse-Project (texlipse.sf.net) and others.
  # All rights reserved. This program and the accompanying materials
  # are made available under the terms of the Eclipse Public License v1.0
  # which accompanies this distribution, and is available at
